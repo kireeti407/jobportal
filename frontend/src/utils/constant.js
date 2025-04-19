b206f72bd4c974276a6a7ea7b4edd3d5e662dada
Kireeti Sangala <136987844+kireeti407@users.noreply.github.com>
2025-04-19 23:03:13 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
-export const USER_API_END_POINT="https://jobportal-back-avsm.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://jobportal-back-avsm.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://jobportal-back-avsm.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://jobportal-back-avsm.onrender.com/api/v1/company";
-export const MESSAGE_API_END_POINT="https://jobportal-back-avsm.onrender.com/api/v1/messages";
+export const USER_API_END_POINT="https://jobportal-fi0z.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://jobportal-fi0z.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://jobportal-fi0z.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://jobportal-fi0z.onrender.com/api/v1/company";
+export const MESSAGE_API_END_POINT="https://jobportal-fi0z.onrender.com/api/v1/messages";
 
