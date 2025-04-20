3ced000cee3cbc2955bc28e45b08bd7e98d3c6f0
Kireeti Sangala <136987844+kireeti407@users.noreply.github.com>
2025-04-20 19:16:31 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -142,7 +142,7 @@ export const loginUser = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpsOnly: true,
-        sameSite: "strict",
+        sameSite: "None",
       })
       .json({
         message: `Welcome back ${user.fullname}`,
